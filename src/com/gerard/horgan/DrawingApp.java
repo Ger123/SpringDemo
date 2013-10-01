9bcf5b209ecb16dbd4c7667e4b09927ffe87e8b8
Ger123 <gerard.horgan1@gmail.com>
2013-10-01 17:45:09 +0100

    using application context

--- src/com/gerard/horgan/DrawingApp.java
+++ src/com/gerard/horgan/DrawingApp.java
@@ -2,15 +2,18 @@ package com.gerard.horgan;
 
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 public class DrawingApp {
 
 	public static void main(String[] args) {
-		//Triangle triangle = new Triangle();// Use Spring instead//
+		//Triangle triangle = new Triangle(); Use Spring instead
 		
-		BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-		Triangle triangle = (Triangle) factory.getBean("triangle");
+		//BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));  Using Application Instead.
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+		Triangle triangle = (Triangle) context.getBean("triangle");
 		triangle.draw();
 	}
 
--- src/com/gerard/horgan/Triangle.java
+++ src/com/gerard/horgan/Triangle.java
@@ -2,8 +2,18 @@ package com.gerard.horgan;
 
 public class Triangle {
 	
+	private String type;
+	
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 	public void draw() {
-		System.out.println("Triangle Drawn");
+		System.out.println(getType()  + " Triangle Drawn");
 	}
 	
 
